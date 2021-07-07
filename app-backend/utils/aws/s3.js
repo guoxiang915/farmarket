9a7f84f0f23dbb2c269d74b40a929147ca879e24
guoxiang915 <guoxiang915@hotmail.com>
2021-07-06 22:51:37 -0400

    Fix credential env value

--- app-backend/utils/aws/s3.js
+++ app-backend/utils/aws/s3.js
@@ -1,14 +1,15 @@
-import { S3, Endpoint } from 'aws-sdk';
+import { S3 } from 'aws-sdk';
 
 const s3 = new S3({
   s3ForcePathStyle: true,
   region: process.env.AWS_REGION || 'localhost',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'S3RVER',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'S3RVER',
+    accessKeyId: process.env.S3_AWS_ACCESS_KEY_ID || 'S3RVER',
+    secretAccessKey: process.env.S3_AWS_SECRET_ACCESS_KEY || 'S3RVER',
   },
 });
 
 console.log(process.env.AWS_ACCESS_KEY_ID);
 
+console.log(process.env.S3_AWS_ACCESS_KEY_ID);
 export default s3;
