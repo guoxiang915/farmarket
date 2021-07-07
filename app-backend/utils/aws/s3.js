54ef3b20bd293c4131469b4b340a0adefa465b3b
guoxiang915 <guoxiang915@hotmail.com>
2021-07-06 22:25:53 -0400

    Fix s3 on prod

--- app-backend/utils/aws/s3.js
+++ app-backend/utils/aws/s3.js
@@ -3,12 +3,12 @@ import { S3, Endpoint } from 'aws-sdk';
 const s3 = new S3({
   s3ForcePathStyle: true,
   region: process.env.AWS_REGION || 'localhost',
-  endpoint:
-    process.env.AWS_S3_ENDPOINT && new Endpoint(process.env.AWS_S3_ENDPOINT),
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'S3RVER',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'S3RVER',
   },
 });
+
+console.log(process.env.AWS_ACCESS_KEY_ID);
 
 export default s3;
